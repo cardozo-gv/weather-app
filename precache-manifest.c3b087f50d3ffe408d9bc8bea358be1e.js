5ef5d2fe8c038336183efbd1f3cd55b2fbdf421c
Your Name <griseldavcardozo@gmail.com>
2019-08-04 17:14:14 -0300

    Updates

---
+++ precache-manifest.c3b087f50d3ffe408d9bc8bea358be1e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "52b58b5a330aac075a61",
+    "url": "/weather-app/static/css/main.fddbf759.chunk.css"
+  },
+  {
+    "revision": "52b58b5a330aac075a61",
+    "url": "/weather-app/static/js/main.52b58b5a.chunk.js"
+  },
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.8d9c99a4.js"
+  },
+  {
+    "revision": "616ca23ca9835d8f7b19",
+    "url": "/weather-app/static/css/2.4c6ea36f.chunk.css"
+  },
+  {
+    "revision": "616ca23ca9835d8f7b19",
+    "url": "/weather-app/static/js/2.616ca23c.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e4e60fd4795b829faa6c816b5ae3dc27",
+    "url": "/weather-app/index.html"
+  }
+];
